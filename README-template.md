db40492ee80c9f56d6f1809b077fdc567d87cccc
hoainiemz <134860094+hoainiemz@users.noreply.github.com>
2024-02-14 02:15:51 +0700

    Initial commit

--- README-template.md
+++ README-template.md
@@ -1,6 +1,6 @@
-# Frontend Mentor - Advice generator app solution
+# Frontend Mentor - Todo app solution
 
-This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
 ## Table of contents
 
@@ -26,7 +26,13 @@ Users should be able to:
 
 - View the optimal layout for the app depending on their device's screen size
 - See hover states for all interactive elements on the page
-- Generate a new piece of advice by clicking the dice icon
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
 
 ### Screenshot
 
--- script.js
+++ script.js
@@ -1,15 +1,91 @@
-const id = document.getElementById('advice-id');
-const advice = document.getElementById('advice-container');
-const dice = document.getElementById('dice-button');
+const container = document.getElementById('todo-container');
+const tmp = document.createElement("div");
+const count = document.querySelector(".count span");
+var dragged, dropped;
+var cnt = 0, d = 0;
 
-async function getAdvice() {
-    let response = await fetch('https://api.adviceslip.com/advice');
-    let received = (await response.json()).slip;
-    // console.log(received);
-    id.innerHTML = `#${received.id}`;
-    advice.innerHTML = `"${received.advice}"`
+function eliminate(x) {
+    while (x.tagName != "DIV") {
+        x = x.parentElement;
+    }
+    container.removeChild(x);
+    cnt--;
+    count.innerHTML = cnt;
 }
 
-getAdvice();
+function moves(a, b){
+    if (a === b) {
+        return;
+    }
+    if (b === null) {
+        container.append(a);
+    }
+    else {
+        container.insertBefore(a, b);
+    }
+}
 
-dice.addEventListener('click', getAdvice);
+function pushes() {
+    const str = document.getElementById('activity').value;
+    if (str.length == 0)
+        return; 
+    cnt++;
+    d++;
+    let pot = document.createElement("div");
+    pot.classList.add('bar');
+    pot.classList.add('todo');
+    pot.setAttribute('draggable', true);
+    pot.innerHTML = `
+    <div class="container">
+      <div class="circle" check=0>
+        <input type="checkbox" class="todo-status" id="checkbox${d}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+      </div>
+      <div class="act"><p><label for="checkbox${d}">${str}</label></p></div>
+    </div>
+    <svg class="eraser" id="num${d}" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+    `;
+    container.insertBefore(pot, container.firstChild);
+    document.getElementById(`num${d}`).addEventListener('click', (x) => {
+        eliminate(x.target);
+    });
+    count.innerHTML = `${cnt}`;
+    document.getElementById('activity').value = '';
+    pot.addEventListener('dragstart', (event) => {
+        dragged = event.target;
+    });
+    pot.addEventListener('drop', (event) => {
+        dropped = event.target;
+        while (dragged.classList.contains('bar') === false) {
+            dragged = dragged.parentElement;
+        }
+        while (dropped.classList.contains('bar') === false) {
+            dropped = dropped.parentElement;
+        }
+        if (dragged === dropped)
+            return;
+        let u = dragged.nextElementSibling, v = dropped.nextElementSibling;
+        moves(dragged, v);
+        moves(dropped, u);
+    });
+    pot.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
+}
+
+function clearcompleted() {
+    document.querySelectorAll(".bar.todo:has(.todo-status:checked)").forEach((x) => {
+        container.removeChild(x);
+        cnt--;
+    });
+    count.innerHTML = `${cnt}`;
+}
+
+document.querySelector('.adder .circle').addEventListener('click', pushes);
+document.querySelector('.count span').innerHTML = `${cnt}`;
+document.getElementById('clear').addEventListener('click', clearcompleted);
+document.getElementById('activity').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        pushes();
+    }
+});
